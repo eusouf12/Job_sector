04e4a11c905a85b99ffa86fb672b5ded353df04a
Eusouf <riyajuddeneusouf@gmail.com>
2024-03-25 14:57:59 +0600

    new update

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -3,8 +3,8 @@
 const Banner = () => {
     return (
         <div>
-           <h2>This is home page banner</h2> 
-           <img src="../../../public/image/images/bg1.png" alt="" />
+           <h2>This is home page banner me i am</h2> 
+           <img src="/image/images/bg1.png" alt="" />
         </div>
     );
 };
