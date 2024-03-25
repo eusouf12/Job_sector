dc9b67db2a4401753080ba4109bbdcedd1b1a61c
Eusouf <riyajuddeneusouf@gmail.com>
2024-03-25 15:49:51 +0600

    add banner section

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -2,9 +2,17 @@
 
 const Banner = () => {
     return (
-        <div>
-           <h2>This is home page banner me i am</h2> 
-           <img src="/image/images/bg1.png" alt="" />
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 py-10 items-center">
+            <div className=" lg:col-span-2 ">
+                <div className="w-full lg:w-[70%] space-y-10">
+                    <h2 className="text-7xl font-extrabold">One Step Closer To Your <span className="text-blue-500"> Dream Job</span></h2>
+                    <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                    <button className="btn bg-blue-300 ">Get Started</button>
+                </div>
+            </div>
+            <div className=" lg:col-span-1">
+                <img src="/image/images/user.png" alt="" />
+            </div>
         </div>
     );
 };
