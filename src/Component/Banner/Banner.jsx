3c4ce23991d6d6c09715e3eaf8abfc4e4d31ac09
Eusouf <riyajuddeneusouf@gmail.com>
2024-03-25 14:45:55 +0600

    add image

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -4,6 +4,7 @@ const Banner = () => {
     return (
         <div>
            <h2>This is home page banner</h2> 
+           <img src="../../../public/image/images/bg1.png" alt="" />
         </div>
     );
 };
